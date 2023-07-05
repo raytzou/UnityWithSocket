e1656d90a0fe78d7d235b348d3963bfdab1c9e72
raytzou <raytzou@outlook.com>
2023-07-06 03:52:02 +0800

    Socket client (bug: one side disconnects may cause another side crash)

--- Assets/Scripts/ChatController.cs
+++ Assets/Scripts/ChatController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System.Threading.Tasks;
+using System;
 
 public class ChatController : MonoBehaviour
 {
@@ -16,6 +17,10 @@ public class ChatController : MonoBehaviour
     TMP_InputField InputBox;
     [SerializeField]
     Button Button;
+    [SerializeField]
+    TMP_Text SignalTextBox;
+    [SerializeField]
+    Image Signal;
 
     private void Start()
     {
@@ -27,31 +32,59 @@ public class ChatController : MonoBehaviour
         Main.GetSingleton.PrintNetInfo();
     }
 
-    private void Update()
+    private async void Update()
     {
         if(Input.GetKeyDown(KeyCode.Return))
         {
             OpenChatToggler = !OpenChatToggler;
 
-            if(!string.IsNullOrEmpty(InputBox.text))
-                OnSubmitMessage();
-
             if (OpenChatToggler)
+            {
                 GetComponent<CanvasGroup>().alpha = 1f;
+                InputBox.Select();
+            }
             else
                 GetComponent<CanvasGroup>().alpha = 0f;
+
+            if (!string.IsNullOrEmpty(InputBox.text))
+                await OnSubmitMessage();
         }
 
         if(Input.GetKeyDown(KeyCode.I)) Main.GetSingleton.PrintNetInfo();
+
+
+        await Main.GetSingleton.ConnectToServer();
+        //Debug.LogError(Main.GetSingleton.IsOnline);
+
+        if (Main.GetSingleton.IsOnline)
+        {
+            Signal.GetComponent<Image>().color = Color.green;
+            SignalTextBox.text = "Status: Online";
+            await Main.GetSingleton.ReceiveMessage();
+        }
+        else // bad... this not gonna happen lol
+        {
+            Signal.GetComponent<Image>().color = Color.red;
+            SignalTextBox.text = "Status: Offline";
+        }
     }
 
-    public void OnSubmitMessage()
+    public async Task OnSubmitMessage()
     {
         if (string.IsNullOrEmpty(InputBox.text)) return; // prevent Button onClick() with no text
 
         ChatMessageQueue.Enqueue(InputBox.text);
-        Main.GetSingleton.SendMessage(InputBox.text);
+        await Main.GetSingleton.SendMessage(InputBox.text);
 
+        UpdateChatBox();
+
+        OpenChatToggler = true;
+        InputBox.text = "";
+        //InputBox.Select();
+    }
+
+    private void UpdateChatBox()
+    {
         TextBox.text = "";
         if (ChatMessageQueue.Count != 0)
         {
@@ -60,8 +93,5 @@ public class ChatController : MonoBehaviour
         }
         if (ChatMessageQueue.Count == 13)
             ChatMessageQueue.Dequeue();
-
-        OpenChatToggler = true;
-        InputBox.text = "";
     }
 }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -5,20 +5,24 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class Main
 {
     private static Main _singleton;
-    public static IPHostEntry _hostEntry;
-    private static IPAddress _ipAddress;
-    private static IPEndPoint _ipEndPoint;
+    private static IPHostEntry _hostEntry;
+    private static IPAddress _addressList;
+    private static IPEndPoint _endPoint;
+    private static Socket _client;
+    private static int _disconnectedTimes = 0;
 
     public string Username { get; set; } = "Unknown";
+    public bool IsOnline { get; set; }
 
     public static Main GetSingleton 
     { 
-        get => _singleton ??= new Main(_hostEntry, _ipAddress, _ipEndPoint); 
+        get => _singleton ??= new Main(_hostEntry, _addressList, _endPoint); 
         set => _singleton = value; 
     }
     
@@ -27,37 +31,72 @@ public class Main
         hostEntry = Dns.GetHostEntry(Dns.GetHostName());
         _hostEntry = hostEntry;
         ipAddress = _hostEntry.AddressList[0];
-        _ipAddress = ipAddress;
+        _addressList = ipAddress;
         ipEndPoint = new(ipAddress, 8787);
-        _ipEndPoint = ipEndPoint;
+        _endPoint = ipEndPoint;
+
+        _client = new(_endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        //ReceiveMessage();
     }
 
     public void PrintNetInfo()
     {
-        Debug.Log("Address: " + _ipEndPoint.Address);
-        Debug.Log("Port Number: " + _ipEndPoint.Port);
+        Debug.Log("Address: " + _endPoint.Address);
+        Debug.Log("Port Number: " + _endPoint.Port);
         Debug.Log("Client Name: " + Username);
     }
 
-    public async void SendMessage(string message) // call void but not Task function in Unity looks safer with async programming, but not normal
+    public async Task ConnectToServer() // calling void but not Task function in Unity looks safer in async programming, but not normal
+    {
+        //_client.Blocking = false; // God knows how I found this shit
+        try
+        {
+            //Debug.LogError(_client.Connected);
+            if (_client.Connected) return;
+
+            await _client.ConnectAsync(_endPoint); // Socket.ConnectAsync is a blocking call, use on your own risk
+            IsOnline = _client.Connected;
+        }
+        catch (Exception ex) 
+        {
+            //Debug.LogError(ex.Message);
+        }
+    }
+
+    public async Task SendMessage(string message)
     {
         if (string.IsNullOrEmpty(message)) return;
 
+        var msgBytes = Encoding.UTF8.GetBytes(Username + "$" + message + "$" + _endPoint.Address);
+        //_client.Blocking = false;
+
+        if (IsOnline)
+            await _client.SendAsync(msgBytes, SocketFlags.None);
+        else
+            Debug.LogError("Can't send a message, client is not online");
+        //client.Close();
+    }
+
+    public async Task ReceiveMessage() // Wish I would know what I'm doing hmm.....
+    {
+        if (!IsOnline)
+        {
+            Debug.LogError("Client is disconnected, please check server side or restart game!");
+            return;
+        }
+
         try
         {
-            using Socket client = new(_ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            var buffer = new byte[256];
+            var receiver = await _client.ReceiveAsync(buffer, SocketFlags.None);
+            var decoder = Encoding.UTF8.GetString(buffer, 0, receiver);
 
-            await client.ConnectAsync(_ipEndPoint);
-
-            var msgBytes = Encoding.UTF8.GetBytes(Username + "$" + message + "$" + _ipEndPoint.Address);
-
-            await client.SendAsync(msgBytes, SocketFlags.None);
-
-            client.Close();
+            Debug.Log(decoder);
         }
-        catch(Exception ex)
+        catch
         {
-            Debug.LogError(ex.Message);
+            // I hate my life
         }
     }
 }
